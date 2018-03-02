2dd2c77ee8f98ea4cb476325db13a3dc5d581716
Shashank <nsshashank91@gmail.com>
2018-03-02 12:51:59 +0530

    wsd v1.7

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var encoding = require('encoding');
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/', function(req, res) {
+  console.log("request received");
+  res.setHeader('Access-Control-Allow-Origin','*');
+});
+
+app.post('/send_save', function(req, res) {
+    var txt = req.body.data;
+    fs.writeFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt','');
+    txt.forEach(function(value){
+        console.log(value);
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.posWord);
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.synWords);
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.gloss);
+		fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
+		fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.example);
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"----------------------------");
+
+    });
+    
+
+    
+
+});
+
+app.listen(3000);
+console.log("server running on 3000");
