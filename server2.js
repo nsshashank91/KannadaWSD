622744a10c5cc24e04d33e07e4bc921d908f4d30
Shashank <nsshashank91@gmail.com>
2018-04-30 12:57:07 +0530

    Single polysemy v1.0

---
+++ server2.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var encoding = require('encoding');
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/', function(req, res) {
+  console.log("request received");
+  res.setHeader('Access-Control-Allow-Origin','*');
+});
+
+app.post('/send_save', function(req, res) {
+    var txt = req.body.data;
+    //fs.writeFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt','');
+	fs.writeFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt','');
+    txt.forEach(function(value){
+        console.log(value);
+		
+		 //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.posWord);
+        //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.synWords);
+        //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.gloss);
+		//fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+		//fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.example);
+        //fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"----------------------------");
+
+        var posWord = value.posWord;
+        console.log(posWord);
+         var targetWord = value.targetWord;
+        console.log(targetWord);
+        var semantic = value.semantic;
+        console.log(semantic);
+
+         fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.posWord);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.targetWord);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.semantic);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"----------------------------");
+    });
+
+});
+
+app.listen(3001);
+console.log("server running on 3001");
