c91cc577fe70a321deefd8bf0258fa8950839f2b
Shashank <nsshashank91@gmail.com>
2018-05-12 07:17:54 +0530

    working single double polysemy

--- server.js
+++ server.js
@@ -12,17 +12,27 @@ app.post('/', function(req, res) {
 
 app.post('/send_save', function(req, res) {
     var txt = req.body.data;
-    fs.writeFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt','');
+    //fs.writeFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt','');
+	fs.writeFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt','');
     txt.forEach(function(value){
         console.log(value);
-        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.posWord);
+        /*fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.posWord);
         fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
         fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.synWords);
         fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
         fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.gloss);
 		fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"::::::");
 		fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',value.example);
-        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"----------------------------");
+        fs.appendFileSync('C:\\Users\\shashank\\workspaceKannada\\KannadaWSD\\message.txt',"----------------------------");*/
+		
+		 fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.posWord);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.synWords);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.gloss);
+		fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"::::::");
+		fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',value.example);
+        fs.appendFileSync('C:\\Users\\shashank\\Documents\\wsd\\wsd\\src\\main\\resources\\message.txt',"----------------------------");
 
     });
     
